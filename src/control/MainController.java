670ca9fcc1a23ecff7e3bdaed598f02c557cf1ff
Jonal Ray Ticug <jonalticug@Jonals-MacBook-Pro.local>
2019-09-23 22:18:19 +0800

    Initial Push for JSP

---
+++ src/control/MainController.java
@@ -0,0 +1,21 @@
+package control;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "control.MainController", urlPatterns = {"/"})
+public class MainController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
+        rd.include(request, response);
+    }
+}
---
+++ web/login.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jonalticug
+  Date: 9/23/19
+  Time: 10:06 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>ANUNA SIRS</title>
+</head>
+<body>
+    <form>
+        <input type="text" name="username"/>
+
+        <input type="submit"/>
+    </form>
+</body>
+</html>
